fe94860b4ad01e1ff3792b961274831ffcd631de
Johny <johnyvozon@gmail.com>
2026-06-10 01:40:14 -0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Sistema-de-Login02/', // Coloque o nome do seu repositório aqui
+  base: '/Sistema-de-Login-02/', // Nome exato do seu repositório no GitHub
 })
